067dad87727fa1d5630241b00441cd4a7ad098ee
CATHERINE MENG <catherine@CATHERINEs-MacBook-Pro.local>
2020-08-04 23:01:07 -0700

    not ignore build folder

---
+++ build/precache-manifest.77591ffab5caf3d73f6fe971f03ed9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faee282684132a3dcdf62c3d8ffc59ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45c4db7e778696d7d9a",
+    "url": "/static/js/2.f21a654d.chunk.js"
+  },
+  {
+    "revision": "851dde5cb57391f99eae94821a738a11",
+    "url": "/static/js/2.f21a654d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2feaeb7d9d801c71bd4",
+    "url": "/static/js/main.e2782232.chunk.js"
+  },
+  {
+    "revision": "efbf7f7cb2e7dc6d3cd5",
+    "url": "/static/js/runtime-main.aa90e938.js"
+  },
+  {
+    "revision": "4f263919aa417201e8987b73776b8cda",
+    "url": "/static/media/uikit.theme.4f263919.less"
+  }
+]);
