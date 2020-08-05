13b8dff548d764b11b96fbc083c0a4b208a175e1
CATHERINE MENG <catherine@CATHERINEs-MacBook-Pro.local>
2020-08-04 23:18:41 -0700

    test heroku server

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var fs = require("fs");
+// var path = require("path");
+var bodyParser = require("body-parser");
 var port = process.env.PORT || 5000;
-// var bodyParser = require("body-parser");
 
 app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
 
 app.get("/posts", function (req, res) {
   const fileName = "public/data.json";
